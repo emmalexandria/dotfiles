21935996950536682d0ae16c3b46814010828c44
Emma Alexandria <emma.jellemabutler@gmail.com>
2024-11-26 16:06:53 +0200

    Fish update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
+abbr -a config "/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME"
+
+set -gx EDITOR nvim
+set -gx SUDO_EDITOR nvim
+set -gx nvm_default_version lts/jod
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
